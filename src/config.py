bfdac8943be6ba9d4d8f59caaed72d2bf50f4ea6
SaiKYouBoi <iliaselggames@gmail.com>
2026-09-24 16:32:43 +0000

    Refactor config.py to correct z-score threshold variable name and add owner mapping

--- src/config.py
+++ src/config.py
@@ -7,5 +7,15 @@ raw_data_path = project_root / "data" / "raw" / "car-price-data.csv"
 
 igr_flag_multiplier = 1.5 
 igr_remove_multiplier = 3.0  
-zscore_threshhold = 3.0
+zscore_threshold = 3.0
 
+current_year = 2026
+min_plausible_year = 1990
+
+
+owner_mapping = {
+    "First Owner": 0,
+    "Second Owner": 1,
+    "Third Owner": 2,
+    "Fourth & Above Owner": 3
+    }
